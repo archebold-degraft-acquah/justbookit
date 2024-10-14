7bf277defe084a98f41749e236a2c5ab810eb911
archebold-degraft-acquah <anandegraft@gmail.com>
2024-10-14 06:11:23 +0000

    Updated value for allowed_host settings in project

--- service_booking_system/settings.py
+++ service_booking_system/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ["web-production-05f8.up.railway.app", "localhost"]
+ALLOWED_HOSTS = ["justbookit.up.railway.app", "localhost"]
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
 # ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
